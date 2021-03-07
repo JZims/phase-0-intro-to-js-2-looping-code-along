d9633d98526cf6114968c84c43ce849138c04289
JZims <JZimmsGames@gmail.com>
2021-03-07 01:15:56 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+ 
+
+ function writeCards(names, event){
+  const newArr = [];
+   for (let i = 0; i < names.length; i++) {
+    
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+     debugger;
+      
+   }
+  return newArr;
+  }
+  //writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+
+
+  function countDown(init){
+    while (init >= 0){
+      console.log(init);
+      init--
+    }
+    
+  }
+countDown(10);
